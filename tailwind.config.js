d252a22e8d0b2365a2f6505632587442582fcbd0
Josias Junior <josias.junior@quintoandar.com.br>
2024-03-26 21:14:12 -0300

    feat: add tailwind css support

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/renderer/src/components/**/*.{ts,tsx}',
+    './src/renderer/src/**/*.{ts,tsx}',
+    './src/renderer/**/*.{html,ts,tsx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
