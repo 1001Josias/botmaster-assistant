6935c815412b437d4ed20f7876f369d29d0c3eeb
Josias Junior <josias.junior@quintoandar.com.br>
2024-04-11 21:18:09 -0300

    feat: allows to execute a process by clicking the 'Start' button in the table

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,10 +1,36 @@
-import { resolve } from 'path'
+import * as path from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import * as fs from 'fs'
+
+const processes = fs.readdirSync('src/bots', { recursive: true, withFileTypes: true })
+const rollupOptionsInput = processes.reduce(
+  (structuredData, file) => {
+    if (file.isDirectory()) return {}
+    const isValidFileName = ['index.ts', 'bot.ts', 'process.ts'].includes(file.name)
+    const filePath = `${file.path}/${file.name}`
+    return isValidFileName
+      ? { ...structuredData, [`bots/${path.basename(file.path)}`]: path.resolve(filePath) }
+      : structuredData
+  },
+  {} as unknown as Record<string, string>
+)
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+
+    build: {
+      rollupOptions: {
+        output: {
+          extend: true
+        },
+        input: {
+          index: path.resolve('src/main/index.ts'),
+          ...rollupOptionsInput
+        }
+      }
+    }
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
@@ -12,7 +38,7 @@ export default defineConfig({
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': path.resolve('src/renderer/src')
       }
     },
     plugins: [react()]
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,8 @@
+import path from 'path'
+import pie from 'puppeteer-in-electron'
 import { app, shell, BrowserWindow, ipcMain } from 'electron'
-import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
+
 import icon from '../../resources/botmaster.png?asset'
 import { getProcesses } from './utils'
 const processes = getProcesses()
@@ -13,7 +15,7 @@ function createWindow(): void {
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
-      preload: join(__dirname, '../preload/index.js'),
+      preload: path.join(__dirname, '../preload/index.js'),
       sandbox: false
     }
   })
@@ -36,35 +38,56 @@ function createWindow(): void {
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+    mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'))
   }
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
+pie
+  .initialize(app)
+  .then(async () => {
+    await app.whenReady()
+    createWindow()
+
+    // Set app user model id for windows
+    electronApp.setAppUserModelId('com.electron')
+
+    // Default open or close DevTools by F12 in development
+    // and ignore CommandOrControl + R in production.
+    // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+    app.on('browser-window-created', (_, window) => {
+      optimizer.watchWindowShortcuts(window)
+    })
+
+    ipcMain.on('run-process', async (event, process) => {
+      try {
+        console.log(`${process.name} started`)
+        const { default: runRobot } = await import(process.path)
+        await runRobot()
+        const successLog = `${process.name} completed!`
+        event.reply(`${process.name}-finished`, successLog)
+        console.log(successLog)
+      } catch (error: unknown) {
+        const failureLog = `${process.name} failed with message:`
+        console.error(failureLog)
+        console.error(error)
+        event.reply(
+          `${process.name}-finished`,
+          `${failureLog} ${error instanceof Error ? error.message : error}`
+        )
+      }
+    })
+
+    app.on('activate', function () {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
   })
-
-  // IPC test
-  ipcMain.on('ping', () => console.log('pong'))
-
-  createWindow()
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+  .catch(console.log)
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
