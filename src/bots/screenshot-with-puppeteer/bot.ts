a3595fddd1a23fcbfe2f469cca8b4ba67d1043ef
Josias <1001josias@gmail.com>
2024-08-07 20:42:14 -0300

    feat(screenshot-with-puppeteer): implement smooth scrolling functionality

--- src/bots/screenshot-with-puppeteer/bot.ts
+++ src/bots/screenshot-with-puppeteer/bot.ts
@@ -3,8 +3,13 @@ import { Page } from 'puppeteer-core'
 export default async function bot({ page }: { page: Page }): Promise<void> {
   try {
     await page.goto('https://www.github.com/1001josias')
-    await new Promise((resolve) => setTimeout(resolve, 5000))
+    await new Promise((resolve) => setTimeout(resolve, 1000))
     await page.screenshot({ path: process.cwd() + `/${Math.random() * 100}-screenshot.png` })
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    for (const _ of Array(330)) {
+      await page.mouse.wheel({ deltaY: 3 })
+      await new Promise((resolve) => setTimeout(resolve, 100))
+    }
     await page.close()
   } catch (error) {
     console.error(error)
